2ffa4413b9d921163a1e0bb507a64a048f0f921d
Zamah Sari <zamahsari@umm.ac.id>
2021-01-10 02:44:03 +0700

    style: :bug: change id to class

--- js/myJavascript.js
+++ js/myJavascript.js
@@ -1,9 +1,10 @@
 $(function() {
-    $('body').on('click', '#myMenu', function(){
+    $('body').on('click', '.myMenu', function(){
         let urlLink=this.href;
-        console.log(urlLink);
+        // console.log(urlLink);
         $.get(urlLink, function(response, status){
-            console.log(status);
+            // console.log(status);
+            // console.log(response);
             $('#content').html(response);
         });
 
